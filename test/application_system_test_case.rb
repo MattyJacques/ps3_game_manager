f45fcb47cf649a0f0ada9d065a409a9ebc30f878
Matthew Jacques <matty.jacques@proton.me>
2026-06-17 20:04:15 +0100

    test: stabilize sqlite-backed system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,10 +4,20 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include ActiveJob::TestHelper
 
   driven_by :rack_test
+  self.use_transactional_tests = false if respond_to?(:use_transactional_tests=)
 
   # rack_test has no JS runtime, so turbo-rails' stream-source wait hook
   # cannot attach to live cable elements during visit.
   def visit(...)
     page.visit(...)
+  end
+
+  teardown do
+    WishlistItem.delete_all
+    MediaFile.delete_all
+    Scan.delete_all
+    Game.delete_all
+    clear_enqueued_jobs
+    clear_performed_jobs
   end
 end
